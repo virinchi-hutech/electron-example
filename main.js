771fe4e0190109146a4c20671516578b56138846
virinchi <uvirinchi16@gmail.com>
2020-09-23 20:45:47 +0530

    Create auto-updating app

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const { autoUpdater } = require("electron-updater");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
 
-function createWindow() {
+function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -11,45 +11,40 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-  mainWindow.loadFile("index.html");
-  mainWindow.on("closed", function () {
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
     mainWindow = null;
   });
-  mainWindow.once("ready-to-show", () => {
+  mainWindow.once('ready-to-show', () => {
     autoUpdater.checkForUpdatesAndNotify();
   });
 }
 
-
-
-
-app.on("ready", () => {
+app.on('ready', () => {
   createWindow();
 });
 
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", function () {
+app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-
-autoUpdater.on("update-available", () => {
-  mainWindow.webContents.send("update_available");
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
 });
 
-autoUpdater.on("update-downloaded", () => {
-  mainWindow.webContents.send("update_downloaded");
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
 });
-
-ipcMain.on("app_version", (event) => {
-  event.sender.send("app_version", { version: app.getVersion() });
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
 });
 
 ipcMain.on('restart_app', () => {
